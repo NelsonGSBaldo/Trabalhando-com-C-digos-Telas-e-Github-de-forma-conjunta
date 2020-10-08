590487b0304347e2d2e89b90ebf727046213d2d5
Nelson Gabriel <61994699+hellzz01@users.noreply.github.com>
2020-10-08 18:01:39 -0300

    Add files via upload

---
+++ cadastrocliente.java
@@ -0,0 +1,7 @@
+package cadastrocliente;
+
+public class cadastrocliente {
+    public static void main(String[] args) {
+        pessoas cliente = new pessoas ("Nelson", 1234567890, "Minha rua", "Sorocaba", "SP");
+    }
+}
